01bd87a94ad490d2c890d5ff14572aed1ebd2212
Ethan <ethan_rosenfeld@horacemann.org>
2025-03-20 00:11:46 -0400

    Cleaning and comments

--- algorithms/newton_method.jl
+++ algorithms/newton_method.jl
@@ -1,40 +1,15 @@
-using GLMakie
+include("../calc_1d.jl")
 
-function f(x)
-    return atan(x)
-end
+f(x) = x^2
 
-function diff(f, x)
-    dx = 1e-10
-    return (f(x + dx) - f(x)) / dx
-end
-
-fig = Figure()
-ax = Axis(fig[1, 1])
-
-xs = collect(LinRange(-2, 2, 1000))
-ys = [f(x) for x in xs]
-
-function newton_method(f, x, iterations)
-    for _ in 1:iterations
+function newton_method(f, x, iters)
+    """ An algorithm to find zeros of a function f by repeatedly finding the x-intercepts of a tangent lines from an initial x value
+    Parameters: f:R -> R, x ∈ R, iters ∈ Z
+    """
+    for _ in 1:iters
         y = f(x)
-        m = diff(f, x)
-
-        scatter!(ax, x, y, color=:blue)
-        lines!(ax, [x, x - y/m], [y, 0], color=:green)
-
+        m = derivative(f, x)
         x = x - (y / m)
-
-        lines!(ax, [x, x], [f(x), 0], color=:green)
-        display(fig)
-        sleep(0.1)
     end
-
     return x
 end
-
-empty!(ax)
-
-lines!(ax, xs, ys, color=:black)
-
-newton_method(f, 1.4, 10)
