205c09f765ae1c1ac56880874c3132bebe1d5f19
Ethan <ethan_rosenfeld@horacemann.org>
2025-03-29 01:23:02 -0400

    Right/left derivative and return comments

--- calc_1d.jl
+++ calc_1d.jl
@@ -1,15 +1,27 @@
+import LinearAlgebra
+
 const Δ = 1e-3
 
 function derivative(f, x)
     """ Evaluate the rate of change of a function f at x 
     Parameters: f:R -> R, x ∈ R
+    Returns: df/dx
     """
-    return (f(x + Δ) - f(x)) / Δ
+    try
+        return (f(x + Δ) - f(x)) / Δ
+    catch
+        try
+            (f(x) - f(x - Δ)) / Δ
+        catch
+            error("f is not defined on [x - Δ, x + Δ]")
+        end
+    end
 end
 
 function integral(f, a, b)
     """ Evaluate the definite integral of f from a to b 
     Parameters: f:R -> R, a,b ∈ R
+    Returns: ∫ f dx
     """
     ys = [f(x) for x in a:Δ:b]
     return sum(ys * Δ)
